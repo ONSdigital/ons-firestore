0cdb3ea3d9b2dc80aa83b8010aa6a74aaf8269f2
John Topley <john.topley@ons.gov.uk>
2022-04-27 15:36:26 +0100

    Incremented patch version

--- lib/ons-firestore/version.rb
+++ lib/ons-firestore/version.rb
@@ -4,7 +4,7 @@ module ONSFirestore
   module Version
     MAJOR = 1
     MINOR = 0
-    TINY  = 1
+    TINY  = 2
   end
   VERSION = [Version::MAJOR, Version::MINOR, Version::TINY].compact * '.'
 end
