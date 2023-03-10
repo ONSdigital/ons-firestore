9f52c78110efd172917c9f3ce86976975852c904
John Topley <john.topley@ons.gov.uk>
2023-03-10 11:43:43 +0000

    Incremented version

--- lib/ons-firestore/version.rb
+++ lib/ons-firestore/version.rb
@@ -4,7 +4,7 @@ module ONSFirestore
   module Version
     MAJOR = 1
     MINOR = 3
-    TINY  = 0
+    TINY  = 1
   end
   VERSION = [Version::MAJOR, Version::MINOR, Version::TINY].compact * '.'
 end
